e0776f03519cfbf44ae3fd5f221cd9a341216a2f
dr-matt-smith <matt.smith@itb.ie>
2020-11-17 19:57:31 +0000

    web xr project

---
+++ Assets/_Scripts/ButtonActions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonActions : MonoBehaviour
+{
+    public void BUTTON_ACTION_reloadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
